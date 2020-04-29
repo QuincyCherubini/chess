02d67f557a436301ed4e03946dbc658f25fa98de
Quincy <Quincy@Cherubini.ca>
2020-04-29 17:23:57 -0400

    added a file for FEN conversion

---
+++ FEN_converter.py
@@ -0,0 +1,13 @@
+################################################################################
+# This file converts a chess position to FEN Notation
+# (https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation)
+#
+# within a game all positions reached are stored in order to check if a draw
+# by repeated positions has occured
+################################################################################
+
+
+def convert_to_FEN(position):
+
+    FEN = ""
+    return FEN
